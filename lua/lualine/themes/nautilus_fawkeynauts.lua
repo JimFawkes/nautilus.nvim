5d7f62d81a3ffd97d1916e912f19cd5e1405ccf2
Jim Fawkes <JimFawkes@users.noreply.github.com>
2023-01-30 17:48:02 +0100

    Change StatusLine BG Color for Fawkeynauts Theme

--- lua/lualine/themes/nautilus_fawkeynauts.lua
+++ lua/lualine/themes/nautilus_fawkeynauts.lua
@@ -6,33 +6,33 @@ local colors = theme.colors
 
 return {
   normal = {
-    a = { bg = colors.bg, fg = colors.white },
+    a = { bg = colors.bg_alt, fg = colors.white },
     b = { bg = colors.purple, fg = colors.white },
-    c = { bg = colors.bg, fg = colors.yellow },
+    c = { bg = colors.bg_alt, fg = colors.yellow },
   },
   insert = {
-    a = { bg = colors.yellow, fg = colors.bg },
+    a = { bg = colors.yellow, fg = colors.bg_alt },
     b = { bg = colors.purple, fg = colors.white },
     c = { bg = colors.vg, fg = colors.yellow }
   },
   visual = {
     a = { bg = colors.magenta, fg = colors.white },
     b = { bg = colors.purple, fg = colors.white },
-    c = { bg = colors.bg, fg = colors.yellow }
+    c = { bg = colors.bg_alt, fg = colors.yellow }
   },
   replace = {
-    a = { bg = colors.bg, fg = colors.white },
+    a = { bg = colors.bg_alt, fg = colors.white },
     b = { bg = colors.purple, fg = colors.white },
-    c = { bg = colors.bg, fg = colors.yellow }
+    c = { bg = colors.bg_alt, fg = colors.yellow }
   },
   command = {
     a = { bg = colors.red, fg = colors.white },
     b = { bg = colors.purple, fg = colors.white },
-    c = { bg = colors.bg, fg = colors.yellow }
+    c = { bg = colors.bg_alt, fg = colors.yellow }
   },
   inactive = {
-    a = { bg = colors.bg, fg = colors.white },
+    a = { bg = colors.bg_alt, fg = colors.white },
     b = { bg = colors.purple, fg = colors.white },
-    c = { bg = colors.bg, fg = colors.yellow }
+    c = { bg = colors.bg_alt, fg = colors.yellow }
   }
 }
